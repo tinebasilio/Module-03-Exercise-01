251be4aa279fba3d241025b2396edcde5d3856d1
tinebasilio <kristinebasilio28@gmail.com>
2024-03-07 23:04:32 +0800

    Add project files.

---
+++ Module03Layout/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Module03Layout
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private async void OnStackLayoutPage(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new StackLayoutPage());
+        }
+
+        private async void OnAbsoluteLayoutPage(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new AbsoluteLayoutPage());
+        }
+        private async void OnRelativeLayoutPage(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new RelativeLayoutPage());
+        }
+        private async void OnGridLayoutPage(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new GridPage());
+        }
+    }
+}
